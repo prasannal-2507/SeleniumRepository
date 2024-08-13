a4f155dfdc99b443448d1708fa9de5722637764e
prasannal-2507 <laxmip.2507@gmail.com>
2024-08-13 18:05:49 +0530

    Add files via upload
    Methods1

---
+++ week1/day1/day2/Library.java
@@ -0,0 +1,16 @@
+package week1.homeAssignment;
+
+public class Library {
+
+	   public String addBook(String bookTitle) {
+		   return "Book Title :"+bookTitle;
+	   }
+	   
+	   public void issueBook() {
+		   System.out.println("Book Issued Succesfully");
+	   }
+		
+
+	}
+
+
